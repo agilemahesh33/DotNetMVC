de038e6d4a9853d4c8b3bcbdc894b5aa6f17d6fc
My Challenge <agilemahesh33@gmail.com>
2024-10-29 13:24:56 +0530

    MVC Initial Empty Project

---
+++ Excercise/MVCTestEmptyProject3/MVCTestEmptyProject3/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTestEmptyProject3.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Index()
+        {
+            return "Hellow from TestController : Index Action";
+        }
+        public string Show()
+        {
+            return "Hello From TestController : Show Action";
+        }
+    }
+}
