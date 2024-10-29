1b1a17f4cd7a04c023e104d789ffa0190a252a73
My Challenge <agilemahesh33@gmail.com>
2024-10-30 04:35:55 +0530

    Controller Scaffolding

---
+++ Excercise/MVCTestEmptyProject3/MVCTestEmptyProject3/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTestEmptyProject3.Controllers
+{
+    public class DemoController : Controller
+    {
+        public string Index()
+        {
+            return "Hellow from Demo Controller : Index Action";
+        }
+        public string Show()
+        {
+            return "Hello From Demo Controller : Show Action";
+        }
+        //// GET: Demo
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
