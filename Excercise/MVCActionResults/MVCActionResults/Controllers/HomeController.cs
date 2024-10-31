da6b8146ba7b1135019469137eff178634118a64
My Challenge <agilemahesh33@gmail.com>
2024-10-31 17:11:08 +0530

    Demo on MVCActionResults

---
+++ Excercise/MVCActionResults/MVCActionResults/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCActionResults.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult Register()
+        {
+            return View();
+        }
+        public ViewResult Login()
+        {
+            return View();
+        }
+    }
+}
