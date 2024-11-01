63403c85798561570dbc40979a72b4b23137ac33
My Challenge <agilemahesh33@gmail.com>
2024-11-01 15:38:38 +0530

    ViewData Example Added

---
+++ Excercise/MVCDataTransfer/MVCDataTransfer/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDataTransfer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index1(int? id, string name, double? price)
+        {
+            ViewData["Id"] = id;
+            ViewData["Name"] = name;
+            ViewData["Price"] = price;
+            return View();
+        }
+        public ViewResult Dispay1()
+        {
+            List<string> Colors = new List<string>() { "Red", "Blue", "Pink", "Black", "White", "Green", "Brown", "Purple" };
+            ViewData["colors"] = Colors;
+            return View();
+        }
+    }
+}
