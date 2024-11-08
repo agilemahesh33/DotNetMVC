b5e0a256b81f313f965b300fe0c82e92d7aca8af
My Challenge <agilemahesh33@gmail.com>
2024-11-08 21:23:45 +0530

    Model Object

--- Excercise/MVCDataTransfer/MVCDataTransfer/Controllers/HomeController.cs
+++ Excercise/MVCDataTransfer/MVCDataTransfer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCDataTransfer.Models;
 
 namespace MVCDataTransfer.Controllers
 {
@@ -13,10 +14,19 @@ namespace MVCDataTransfer.Controllers
         /*
         Drawbacks of ViewData:
     1.	ViewData can transfer data from a Controllers - Action method to its corresponding View only.
-    2.	ViewData life lasts only during the current HTTP request i.e., ViewData values will be cleared if redirection occurs.
-    3.	ViewData is resolved dynamically at runtime, as a result, it doesn’t provide any compile-time error checking as well as we do not get support of Intellisense. For example, if we miss-spell the “Key Names” then we will not get any compile-time or runtime error also, whereas we come to know about the problem at the runtime because the value is not displayed.
-    4.	ViewData values must be converted into an appropriate type (un-boxing) before using them because they are present in object format, as we have performed in-case of Price value to calculate 10% Tax.
-    Note: we can store scalar as well as complex types also in ViewData but while accessing them we need to explicitly convert them into its original type again. To test this, add a new Action method in “HomeController” class as below:
+    2.	ViewData life lasts only during the current HTTP request 
+        i.e., ViewData values will be cleared if redirection occurs.
+    3.	ViewData is resolved dynamically at runtime, as a result, 
+        it doesn’t provide any compile-time error checking as well as 
+        we do not get support of Intellisense. For example, if we miss-spell the “Key Names” 
+        then we will not get any compile-time or runtime error also, 
+        whereas we come to know about the problem at the runtime because the value is not displayed.
+    4.	ViewData values must be converted into an appropriate type (un-boxing) before using them 
+        because they are present in object format, as we have performed 
+        in-case of Price value to calculate 10% Tax.
+    Note: we can store scalar as well as complex types also in ViewData 
+        but while accessing them we need to explicitly convert them into its original type again. 
+        To test this, add a new Action method in “HomeController” class as below:
 
         */
         public ViewResult Index1(int? id, string name, double? price)
@@ -63,5 +73,23 @@ namespace MVCDataTransfer.Controllers
             return View();
         }
         #endregion ViewBag
+
+
+        #region Model Object
+        public ViewResult Index15(int? id, string name, double? price)
+        {
+            Products prod = new Products {  Id = id, Name = name, Price = price };
+            return View(prod);
+           
+        }
+        public RedirectToRouteResult Index16(Products prod)
+        {
+            return RedirectToAction("Index6", "Test", prod);
+        }
+        public ViewResult Index6(Products prod)
+        {
+            return View(prod);
+        }
+        #endregion Model Object
     }
 }
