6cfab6c656c6e37e09a1cc0aa740675aa9b590cf
My Challenge <agilemahesh33@gmail.com>
2024-11-09 09:32:34 +0530

    UI Designing using Simple HTML

---
+++ Excercise/MVCUIDesigning/MVCUIDesigning/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCUIDesigning.Models;
+
+namespace MVCUIDesigning.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public ViewResult AddEmployee()
+        {
+            return View();
+        }
+
+        //Way 1
+        //[HttpPost]
+        //public ViewResult AddEmployee(int? Id, string Name, string Job, double? Salary)
+        //{
+        //    ViewData["Id"]=Id;
+        //    ViewData["Name"]=Name;
+        //    ViewData["Job"] = Job;
+        //    ViewData["Salary"] = Salary;
+        //    return View("DisplayEmp1");
+        //}
+
+        //Way 2
+        //[HttpPost]
+        //public ViewResult AddEmployee (FormCollection formCollection)
+        //{
+        //    ViewData["Id"] = formCollection["Id"];
+        //    ViewData["Name"] = formCollection["Name"];
+        //    ViewData["Job"] = formCollection["Job"];
+        //    ViewData["Salary"] = formCollection["Salary"];
+        //    return View("DisplayEmp1");
+        //}
+
+        //Way 3 :Model Binding, Include namespace controller and in view as well.
+        [HttpPost]
+        public ViewResult AddEmployee(Employee employee)
+        {            
+            return View("DisplayEmp2",employee);
+        }
+
+    }
+}
---
+++ Excercise/MVCUIDesigning/MVCUIDesigning/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCUIDesigning.Models
+{
+    public class Employee
+    {
+        //Names must match with names in the controller
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Job {  get; set; }
+        public double? Salary { get; set; }
+    }
+}
