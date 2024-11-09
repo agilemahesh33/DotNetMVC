0b5016b8460592415e59c313eba0a2894871a4b6
My Challenge <agilemahesh33@gmail.com>
2024-11-09 14:28:30 +0530

    HTML Helper Methods and Bootstrap Integration

---
+++ Excercise/MVCUIDesigning/MVCUIDesigning/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCUIDesigning.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Validate()
+        {
+            string uname = Request["txtName"];
+            string password = Request["txtPwd"];
+            if (uname == "Mahesh" && password == "3303")
+            {
+                Session["username"] = uname;
+                return View("Success");
+            }
+            else
+            {
+                ViewData["username"] = uname;
+                return View("Failure");
+            }
+        }        
+    }
+}
