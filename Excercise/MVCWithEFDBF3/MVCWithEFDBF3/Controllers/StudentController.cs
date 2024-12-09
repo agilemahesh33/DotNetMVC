c9eb73f805a21c6974225fe1b2b2d8cc33af16c2
My Challenge <agilemahesh33@gmail.com>
2024-12-09 21:08:08 +0530

    MVC With EF DBF 3 Using Stored Procedure CRUD

---
+++ Excercise/MVCWithEFDBF3/MVCWithEFDBF3/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCWithEFDBF3.Models;
+
+namespace MVCWithEFDBF3.Controllers
+{
+    public class StudentController : Controller
+    {
+        MVCDBEntities dc = new MVCDBEntities();
+        public ViewResult DisplayStudents()
+        {
+            var students = dc.Student_Select(null, true);
+            return View(students);
+        }
+    }
+}
---
+++ Excercise/MVCWithEFDBF3/MVCWithEFDBF3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWithEFDBF3.MvcApplication" Language="C#" %>
