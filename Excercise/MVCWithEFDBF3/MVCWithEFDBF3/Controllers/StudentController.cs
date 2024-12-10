26eb8a7f3bb3d517d247bb9115a3ab57ba9e9d74
My Challenge <agilemahesh33@gmail.com>
2024-12-10 10:22:00 +0530

    EF DBF Working with Procedures

--- Excercise/MVCWithEFDBF3/MVCWithEFDBF3/Controllers/StudentController.cs
+++ Excercise/MVCWithEFDBF3/MVCWithEFDBF3/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.IO;
 using System.Web.Mvc;
 using MVCWithEFDBF3.Models;
 
@@ -12,8 +13,66 @@ namespace MVCWithEFDBF3.Controllers
         MVCDBEntities dc = new MVCDBEntities();
         public ViewResult DisplayStudents()
         {
-            var students = dc.Student_Select(null, true);
-            return View(students);
+            return View(dc.Student_Select(null, true).ToList());
+        }
+        public ViewResult DisplayStudent(int SID)
+        {
+            return View(dc.Student_Select(SID, true).Single());
+        }
+        [HttpGet]
+        public ViewResult AddStudent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public RedirectToRouteResult AddStudent(Student_Select_Result student, HttpPostedFileBase selectedFile)
+        {
+            if (selectedFile != null)
+            {
+                string directoryPath = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(directoryPath))
+                    Directory.CreateDirectory(directoryPath);
+                selectedFile.SaveAs(directoryPath + selectedFile.FileName);
+                student.Photo = selectedFile.FileName;
+            }
+            dc.Student_Insert(student.Sid, student.Name, student.Class, student.Fees, student.Photo);
+            return RedirectToAction("DisplayStudents");
+        }
+        [HttpGet]
+        public ViewResult EditStudent(int SID)
+        {
+            var student = dc.Student_Select(SID, true).Single();
+            TempData["Photo"] = student.Photo;
+            return View(student);
+        }
+        [HttpPost]
+        public RedirectToRouteResult EditStudent(Student_Select_Result student, HttpPostedFileBase selectedFile)
+        {
+            if (selectedFile != null)
+            {
+                string directoryPath = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(directoryPath))
+                    Directory.CreateDirectory(directoryPath);
+                selectedFile.SaveAs(directoryPath + selectedFile.FileName);
+                student.Photo = selectedFile.FileName;
+            }
+            else if (TempData["Photo"]!=null)
+            {
+                student.Photo = TempData["Photo"].ToString();
+            }
+            dc.Student_Update(student.Sid, student.Name, student.Class, student.Fees, student.Photo);
+            return RedirectToAction("DisplayStudents");
+        }
+        [HttpGet]
+        public ViewResult DeleteStudent(int SID)
+        {
+            return View(dc.Student_Select(SID, true).Single());
+        }
+        [HttpPost]
+        public RedirectToRouteResult DeleteStudent(Student_Select_Result student)
+        {
+            dc.Student_Delete(student.Sid);
+            return RedirectToAction("DisplayStudents");
         }
     }
 }
