4af09aafa7faa3a3e5bd439ee1936f7eeaa5ba69
My Challenge <agilemahesh33@gmail.com>
2024-12-11 19:09:26 +0530

    MVC With EF CF Added Seed Data Class for initialization

--- Excercise/MVCWithEFCF2/MVCWithEFCF2/Controllers/SupplierController.cs
+++ Excercise/MVCWithEFCF2/MVCWithEFCF2/Controllers/SupplierController.cs
@@ -11,20 +11,7 @@ namespace MVCWithEFCF2.Controllers
     {
         CompanyDBContext dc = new CompanyDBContext();
         public ActionResult Index()
-        {
-            Supplier s1 = new Supplier { Sid = 131, SupplierName = "Ashok Distributor" };
-            Supplier s2 = new Supplier { Sid = 132, SupplierName = "Meghna Distributor" };
-            Supplier s3 = new Supplier { Sid = 133, SupplierName = "Diamond Distributor" };
-            Supplier s4 = new Supplier { Sid = 134, SupplierName = "Prathmesh Distributor" };
-            Supplier s5 = new Supplier { Sid = 135, SupplierName = "Arihant Distributor" };
-            Supplier s6 = new Supplier { Sid = 136, SupplierName = "Prasad Distributor" };
-            dc.Suppliers.Add(s1);
-            dc.Suppliers.Add(s2);
-            dc.Suppliers.Add(s3);
-            dc.Suppliers.Add(s4);
-            dc.Suppliers.Add(s5);
-            dc.Suppliers.Add(s6);
-            dc.SaveChanges();
+        {           
             return View(dc.Suppliers);
         }
     }
---
+++ Excercise/MVCWithEFCF2/MVCWithEFCF2/Models/CompanyDBInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCWithEFCF2.Models
+{
+    public class CompanyDBInitializer : DropCreateDatabaseAlways<CompanyDBContext>
+    {
+        protected override void Seed(CompanyDBContext context)
+        {
+            Supplier s1 = new Supplier { Sid = 131, SupplierName = "Ashok Distributor" };
+            Supplier s2 = new Supplier { Sid = 132, SupplierName = "Meghna Distributor" };
+            Supplier s3 = new Supplier { Sid = 133, SupplierName = "Diamond Distributor" };
+            Supplier s4 = new Supplier { Sid = 134, SupplierName = "Prathmesh Distributor" };
+            Supplier s5 = new Supplier { Sid = 135, SupplierName = "Arihant Distributor" };
+            Supplier s6 = new Supplier { Sid = 136, SupplierName = "Prasad Distributor" };
+            context.Suppliers.Add(s1);
+            context.Suppliers.Add(s2);
+            context.Suppliers.Add(s3);
+            context.Suppliers.Add(s4);
+            context.Suppliers.Add(s5);
+            context.Suppliers.Add(s6);
+            context.SaveChanges();
+        }
+    }
+}
