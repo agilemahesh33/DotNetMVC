5df8a26498c7c1e388f53d958df339040c27b73a
My Challenge <agilemahesh33@gmail.com>
2024-12-11 07:44:08 +0530

    MVC with EF Code First few minor changes required

---
+++ Excercise/MVCWithEFCF1/MVCWithEFCF1/Models/StoreDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCWithEFCF1.Models
+{
+    public class StoreDBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
