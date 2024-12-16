b7650971c596fde6af65e5f95b72c43da17d66c7
My Challenge <agilemahesh33@gmail.com>
2024-12-16 12:05:50 +0530

    MVC Filters ChildActionOnly Implemented

---
+++ Excercise/MVCFilters/MVCFilters/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using MVCFilters.Models;
+using System.Linq;
+using System.Web.Caching;
+
+namespace MVCFilters.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCDBEntities dc = new MVCDBEntities();
+        #region ChildActionOnly Filter
+        public ViewResult DisplayDepts()
+        {
+            return View(dc.Departments);
+        }
+        [ChildActionOnly]
+        public ViewResult DisplayEmpByDID(int DID)
+        {
+            var EmpdeptID = from E in dc.Employees where E.Did == DID select E;
+            return View(EmpdeptID);
+        }
+        #endregion ChildActionOnly Filter
+
+        #region OutputCache Filter
+        public ViewResult DisplayCustomers1()
+        {
+            return View(dc.Customers);
+        }
+        #endregion OutputCache Filter
+    }
+}
