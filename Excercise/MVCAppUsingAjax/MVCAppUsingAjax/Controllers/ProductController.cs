2a5922d01783b5cc66ad44a264bfd8d9dd06f6a3
My Challenge <agilemahesh33@gmail.com>
2024-12-19 11:33:58 +0530

    Autocomplete Sorting of Products W/O JQuery & Ajax

---
+++ Excercise/MVCAppUsingAjax/MVCAppUsingAjax/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCAppUsingAjax.Models;
+
+namespace MVCAppUsingAjax.Controllers
+{
+    public class ProductController : Controller
+    {
+        NorthwindEntities dc = new NorthwindEntities();
+        // GET: Product
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ViewResult DisplayProducts()
+        {
+            return View(dc.Products);
+        }
+        public ViewResult SearchProduct(string SearchTerm)
+        {
+            List<Product> products = new List<Product>();
+            if (string.IsNullOrEmpty(SearchTerm))
+            {
+                products = dc.Products.ToList();
+            }
+            else
+            {
+                //products = (from P in dc.Products where P.ProductName.Contains(SearchTerm) select P).ToList();
+                products = dc.Products.Where(P => P.ProductName.Contains(SearchTerm)).ToList();
+            }
+            return View("DisplayProducts", products);
+        }
+        public JsonResult GetProductsName(string term)
+        {
+            //var ProductNameList = (from P in dc.Products where P.ProductName.StartsWith(term) select new { P.ProductName }).ToList();
+            var ProductNameList = dc.Products.Where(P => P.ProductName.StartsWith(term)).Select(P => P.ProductName).ToList();
+            return Json(ProductNameList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
