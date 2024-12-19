81c952bedbc5d6bf96b421ab45122f2188f9d07d
My Challenge <agilemahesh33@gmail.com>
2024-12-19 18:28:48 +0530

    WebAPI Service Simple CRUD on Static List

---
+++ Excercise/WebAPIService/WebAPIService/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIService.Controllers
+{
+    public class TestController : ApiController
+    {
+        //As it is made static its single copy for every request
+        //i.e. for all users only one copy will be available
+        static List<string> Colors = new List<string>()
+        {
+            "Red","Blue","Green","Purple"
+        };
+        public IEnumerable<string> Get()
+        {
+            return Colors;
+        }
+        public string Get(int Id)
+        {
+            return Colors[Id];
+        }
+        public void post([FromBody] string color)
+        {
+            Colors.Add(color);
+        }
+        public void put(int Id, [FromBody] string color)
+        {
+            Colors[Id] = color;
+        }
+        public void delete(int Id)
+        {
+            Colors.RemoveAt(Id);
+        }
+    }
+}
