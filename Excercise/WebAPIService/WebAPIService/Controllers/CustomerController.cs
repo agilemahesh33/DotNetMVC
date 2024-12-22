4ab4fcf88ac87a5743387b31d6e3bbdad278c12a
My Challenge <agilemahesh33@gmail.com>
2024-12-22 22:38:54 +0530

    WebAPIService API Call Using Ajax-JQuery-without Postback

---
+++ Excercise/WebAPIService/WebAPIService/Controllers/CustomerController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIService.Models;
+
+namespace WebAPIService.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        MVCDBEntities dc = new MVCDBEntities();
+        //public IEnumerable<Customer> GetCustomers()
+        //{
+        //    //no need of conversion because of IEnumerable
+        //    var customer = dc.Customers;//.Where(P => P.Status == true); Handled at front end during API Call
+        //    return customer;
+        //}
+        //http://localhost:58069/api/Customer/
+        public List<Customer> GetCustomers()
+        {
+            //no need of conversion because of IEnumerable
+            var customer = dc.Customers;//.Where(P => P.Status == true); Handled at front end during API Call
+            return customer.ToList();
+        }
+        public Customer GetCustomer(int id)
+        {
+            return (dc.Customers.Find(id));
+        }
+        public HttpResponseMessage Post(Customer cust)
+        {
+            try
+            {
+                cust.Status = true;
+                dc.Customers.Add(cust);
+                dc.SaveChanges();
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            catch (Exception)
+            {
+                throw new HttpResponseException(HttpStatusCode.InternalServerError);
+            }
+        }
+        public HttpResponseMessage Put(Customer cust)
+        {
+            try
+            {
+                Customer customer = dc.Customers.Find(cust.CustId);
+                if (customer == null)
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                }
+                customer.Name = cust.Name;
+                customer.Balance = cust.Balance;
+                customer.City = cust.City;
+                dc.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                dc.SaveChanges();
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            catch (Exception)
+            {
+                throw new HttpResponseException(HttpStatusCode.InternalServerError);
+            }
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                Customer customer = dc.Customers.Find(id);
+                if (customer == null)
+                {
+                    return new HttpResponseMessage(HttpStatusCode.NotFound);
+                }
+                customer.Status = false;
+                dc.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                dc.SaveChanges();
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            catch (Exception)
+            {
+              throw new HttpResponseException(HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
--- Excercise/WebAPIService/WebAPIService/Global.asax.cs
+++ Excercise/WebAPIService/WebAPIService/Global.asax.cs
@@ -12,6 +12,7 @@ namespace WebAPIService
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
