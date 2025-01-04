a242147c0d37a97e8b31a4d97b1b35b157d93449
My Challenge <agilemahesh33@gmail.com>
2025-01-04 15:12:48 +0530

    Core WebApi Service XML CRUD Created and Tested with Swagger

---
+++ Excercise/CoreWebApiService/CoreWebApiService/Models/ICustomerDAL.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreWebApiService.Models
+{
+    public interface ICustomerDAL
+    {
+        List<Customer> Customers_Select();
+        Customer Customer_Select(int Custid);
+        void Customer_Insert(Customer customer);
+        void Customer_Update(Customer customer);
+        void Customer_Delete(int Custid);
+    }
+}
