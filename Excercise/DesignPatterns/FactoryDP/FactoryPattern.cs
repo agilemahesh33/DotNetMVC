cf8ae6ecf8401b2f87ee9a6e1232b61426c26486
Mahesh M Baradkar <agilemahesh33@gmail.com>
2025-04-26 20:09:25 +0530

    Factory Design Pattern

---
+++ Excercise/DesignPatterns/FactoryDP/FactoryPattern.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDP
+{
+    public interface IPizza //Iproduct
+    {
+        string Eat();
+    }
+    //Concrete Implementation VegPizza
+    class VegPizza : IPizza
+    {
+        public string Eat()
+        {
+            return "Eating Veg Pizza !!!!";
+        }
+    }
+    //Concrete Implementation NonVegPizza
+    class NonVegPizza : IPizza
+    {
+        public string Eat()
+        {
+            return "Eating NonVeg Pizza ): ";
+        }
+    }
+    public interface IPizzaChef //Factory
+    {
+        IPizza PreparePizza();
+    }
+    class VegPizzaChef : IPizzaChef
+    {
+        public IPizza PreparePizza()
+        {
+            return new VegPizza();
+        }
+    }
+    class NonVegPizzaChef : IPizzaChef
+    {
+        public IPizza PreparePizza()
+        {
+            return new NonVegPizza();
+        }
+    }
+
+    class Waiter //Client Class 
+    {
+        public IPizza GetPizza(string type)
+        {
+            IPizzaChef chef;
+            switch (type)
+            {
+                case "Veg":
+                    chef = new VegPizzaChef();
+                    break;
+                case "NonVeg":
+                    chef = new NonVegPizzaChef();
+                    break;
+                default:
+                    throw new ArgumentException("Invalid pizza type");
+            }
+            return chef.PreparePizza();
+        }
+    }
+}
---
+++ Excercise/DesignPatterns/FactoryDP/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FactoryDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Waiter waiter = new Waiter();
+            IPizza pizza = waiter.GetPizza("Veg");
+            Console.WriteLine(pizza.Eat());
+
+            pizza = waiter.GetPizza("NonVeg");
+            Console.WriteLine(pizza.Eat());
+
+            //pizza = waiter.GetPizza("MashroomVeg"); //Will Throw Exception
+            //Console.WriteLine(pizza.Eat());
+            
+            //Or
+
+            Console.WriteLine("Enter the type of pizza (Veg/NonVeg):");
+            Console.WriteLine(waiter.GetPizza(Console.ReadLine()).Eat());
+        }
+    }
+}
