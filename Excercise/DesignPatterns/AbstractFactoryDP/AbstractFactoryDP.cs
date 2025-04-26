4f43ee3cbcbf199ca7efabd1bb71da061d86794d
Mahesh M Baradkar <agilemahesh33@gmail.com>
2025-04-26 20:13:19 +0530

    Abstract Factory DP Example

---
+++ Excercise/DesignPatterns/AbstractFactoryDP/AbstractFactoryDP.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryDP
+{
+    public interface IPizza //IproductA
+    {
+        string Eat();
+    }
+    //Concrete Implementation VegPizza
+    class VegPizza : IPizza //ConcreteProduct1A
+    {
+        public string Eat()
+        {
+            return "Eating Veg Pizza !!!!";
+        }
+    }
+    //Concrete Implementation NonVegPizza
+    class NonVegPizza : IPizza //ConcreteProduct2A
+    {
+        public string Eat()
+        {
+            return "Eating NonVeg Pizza ): ";
+        }
+    }
+    public interface IPizzaChef //Factory
+    {
+        IPizza PreparePizza();
+    }
+    class VegPizzaChef : IPizzaChef
+    {
+        public IPizza PreparePizza()
+        {
+            return new VegPizza();
+        }
+    }
+    class NonVegPizzaChef : IPizzaChef
+    {
+        public IPizza PreparePizza()
+        {
+            return new NonVegPizza();
+        }
+    }
+    //
+    public interface IBurger //IproductB
+    {
+        string Eat();
+    }
+    //Concrete Implementation VegBurger
+    class VegBurger : IBurger //ConcreteProduct1B
+    {
+        public string Eat()
+        {
+            return "Eating Veg Burger !!!!";
+        }
+    }
+    //Concrete Implementation NonVegBurger
+    class NonVegBurger : IBurger //ConcreteProduct2B
+    {
+        public string Eat()
+        {
+            return "Eating NonVeg Burger ): ";
+        }
+    }
+    public interface IBurgerChef //Factory
+    {
+        IBurger PrepareBurger();
+    }
+    class VegBurgerChef : IBurgerChef
+    {
+        public IBurger PrepareBurger()
+        {
+            return new VegBurger();
+        }
+    }
+    class NonVegBurgerChef : IBurgerChef
+    {
+        public IBurger PrepareBurger()
+        {
+            return new NonVegBurger();
+        }
+    }
+    interface IChef //Factory
+    {
+        IPizza PreparePizza();
+        IBurger PrepareBurger();
+    }
+    //Products
+    class VegChef : IChef
+    {
+        public IPizza PreparePizza()
+        {
+            return new VegPizza();
+        }
+        public IBurger PrepareBurger()
+        {
+            return new VegBurger();
+        }
+    }
+    //Products
+    class NonVegChef : IChef
+    {
+        public IBurger PrepareBurger()
+        {
+            return new NonVegBurger();
+        }
+
+        public IPizza PreparePizza()
+        {
+            return new NonVegPizza();
+        }
+    }
+    //New Way of Implementing using Generics 
+    class Waiter
+    {
+        private static readonly Dictionary<string, Func<IChef>> chefs = new()
+        {
+            { "Veg", () => new VegChef() },
+            { "NonVeg", () => new NonVegChef() }
+        };
+
+        private IChef foodFactory;
+
+        public Waiter(string preference)
+        {
+            if (!chefs.TryGetValue(preference, out var chefFactory))
+                throw new ArgumentException("Unknown preference: " + preference);
+            foodFactory = chefFactory();
+        }
+
+        public IPizza GetPizza()
+        {
+            return foodFactory.PreparePizza();
+        }
+
+        public IBurger GetBurger()
+        {
+            return foodFactory.PrepareBurger();
+        }
+    }
+
+    //Simplest and formal way to call
+    //class Waiter
+    //{
+    //    private IChef foodFactory;
+    //    public Waiter(string preference)
+    //    {
+    //        if (preference == "Veg")
+    //            foodFactory = new VegChef();
+    //        else
+    //            foodFactory = new NonVegChef();
+    //    }
+    //    public IPizza GetPizza()
+    //    {
+    //        return foodFactory.PreparePizza();
+    //    }
+    //    public IBurger GetBurger()
+    //    {
+    //        return foodFactory.PrepareBurger();
+    //    }
+    //}
+}
---
+++ Excercise/DesignPatterns/AbstractFactoryDP/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AbstractFactoryDP
+{
+    internal class Program
+    {
+        //New Way of Implementing using Generics 
+        static void Main(string[] args)
+        {
+            // Create a waiter by specifying preference
+            Waiter waiter = new Waiter("Veg");
+
+            // Ask for Pizza and Burger
+            IPizza pizza = waiter.GetPizza();
+            Console.WriteLine(pizza.Eat());
+
+            IBurger burger = waiter.GetBurger();
+            Console.WriteLine(burger.Eat());
+        }
+
+        //Simplest and formal way to call
+        //static void Main(string[] args)
+        //{
+        //    Waiter waiter = new Waiter("NonVeg");
+        //    IPizza pizza = waiter.GetPizza();
+        //    Console.WriteLine(pizza.Eat());
+        //    IBurger burger = waiter.GetBurger();
+        //    Console.WriteLine(burger.Eat());
+        //}
+
+    }
+}
