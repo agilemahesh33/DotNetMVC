878c33565e9a22a7a83285161512152b5d3d6d4e
Mahesh M Baradkar <agilemahesh33@gmail.com>
2025-04-28 17:47:48 +0530

    Prototype Design Pattern Added

---
+++ Excercise/DesignPatterns/PrototypeDP/Address.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeDP
+{
+    //Complex Object
+    public class Address
+    {
+        public string Street {  get; set; }
+        public string City { get; set; }
+        public Address(string street, string city)
+        {
+            Street = street;
+            City = city;
+        }
+        public Address DeepCopy()
+        {
+            return new Address(Street, City);
+        }
+    }
+    //Prototype Class
+    public class Person : ICloneable
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+        public Person(String Name, Address address)
+        {
+            this.Name = Name;
+            this.Address = address;
+        }
+        //Shallow Copy (default)
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+        //Deep Copy (Custom)
+        public Person DeepClone()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.Address = this.Address.DeepCopy();// Important: deep clone Address separately
+            return clone;   //new Person(Name, Address);
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Name: {Name}, Street: {Address.Street},City: {Address.City}");
+        }
+    }
+}
---
+++ Excercise/DesignPatterns/PrototypeDP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System;
+
+namespace PrototypeDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Original object
+            Person original = new Person("John Doe", new Address("123 Main St", "New York"));
+
+            // Shallow copy
+            Person shallowCopy = (Person)original.Clone();
+
+            // Deep copy
+            Person deepCopy = original.DeepClone();
+
+            Console.WriteLine("Before Modification:");
+            original.Display();
+            shallowCopy.Display();
+            deepCopy.Display();
+
+            // Modify original object's Address
+            original.Address.Street = "999 Changed St";
+
+            Console.WriteLine("\nAfter Modification:");
+            original.Display();      // Address changed
+            shallowCopy.Display();   // Address also changed (because of shallow copy)
+            deepCopy.Display();      // Address NOT changed (because of deep copy)
+
+            Console.ReadKey();
+        }
+    }
+}
