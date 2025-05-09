64bafb009b357e725332c08a72532e40d290ae1a
Mahesh M Baradkar <agilemahesh33@gmail.com>
2025-05-09 18:43:25 +0530

    Singleton DP added

---
+++ Excercise/DesignPatterns/SingletonDP/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SingletonDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton abc = new Singleton();//Error inaccessible due to protection level
+            Singleton inst = Singleton.getInstance();//Direct Access because of static
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Excercise/DesignPatterns/SingletonDP/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDP
+{
+    internal class Singleton
+    {
+        private Singleton() { }
+        private static Singleton instance;
+        // if used = new Singleton(); then always initializes the instance even
+        // though not called i.e. not lazy
+
+        private static object instanceLock = new object(); //To Create lock
+        public static Singleton getInstance()
+        {
+            if (instance == null) //First check to avoid multi thread waiting for lock release
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null) //if null only then create
+                    {
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;            
+        }
+    }
+}
